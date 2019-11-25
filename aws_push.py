eaba4e03135d58ac35032f79cef8e447a598f283
Andreas Ulm <andreas.ulm@root360.de>
2019-11-25 12:12:57 +0100

    fixed syntax of aws_push.py based on pylint
    Fixed issues reported by pylint as part of
    https://github.com/cloudflare/flan/issues/27.
    
    Signed-off-by: Andreas Ulm <andreas.ulm@root360.de>

--- aws_push.py
+++ aws_push.py
@@ -1,13 +1,18 @@
-import boto3
 import sys
 import os
+import boto3
 
-filename = sys.argv[1]
 
-s3_client = boto3.client('s3')
-try:
-    response = s3_client.upload_file("/" + filename, os.getenv('bucket'),
-                                     filename)
-except Exception, e:
-    print('Error uploading to s3')
-    print(e)
+def main():
+    filename = sys.argv[1]
+
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.upload_file("/" + filename, os.getenv('bucket'), filename)
+    except Exception as err:
+        print('Error uploading to s3')
+        print(err)
+
+
+if __name__ == "__main__":
+    main()
