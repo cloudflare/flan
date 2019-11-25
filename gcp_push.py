d2fd01646dc6f7f524d5c8f9ecd2670360e9c0d9
Andreas Ulm <andreas.ulm@root360.de>
2019-11-25 12:15:06 +0100

    Fixed issues reported by pylint as part of https://github.com/cloudflare/flan/issues/27.
    Signed-off-by: Andreas Ulm <andreas.ulm@root360.de>

--- gcp_push.py
+++ gcp_push.py
@@ -1,10 +1,16 @@
-from google.cloud import storage
 import sys
 import os
+from google.cloud import storage
 
-filename = sys.argv[1]
 
-client = storage.Client()
-bucket = client.bucket(os.getenv('bucket'))
-blob = bucket.blob(filename)
-blob.upload_from_filename(filename)
+def main():
+    filename = sys.argv[1]
+
+    client = storage.Client()
+    bucket = client.bucket(os.getenv('bucket'))
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)
+
+
+if __name__ == "__main__":
+    main()
