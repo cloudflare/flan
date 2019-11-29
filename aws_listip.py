e2f382c43d4b201ce843f4013eeb613109e812c4
ekapratama93 <ekapratama93@gmail.com>
2019-11-29 17:10:47 +0700

    fix no region

--- aws_listip.py
+++ aws_listip.py
@@ -1,18 +1,24 @@
 import boto3
 
 
-ec2 = boto3.resource('ec2')
-
-running_instances = ec2.instances.filter(Filters=[{
-    'Name': 'instance-state-name',
-    'Values': ['running']}])
+session = boto3.Session(region_name="us-east-1")
+client = session.client('ec2')
+regions = client.describe_regions()
 
 ips = []
-for instance in running_instances:
-    if instance.public_ip_address:
-        ips.append(instance.public_ip_address)
-    else:
-        ips.append(instance.private_ip_address)
+
+for region in regions['Regions']:
+    session = boto3.Session(region_name=region['RegionName'])
+    ec2 = session.resource('ec2')
+    running_instances = ec2.instances.filter(Filters=[{
+            'Name': 'instance-state-name',
+            'Values': ['running']}])
+
+    for instance in running_instances:
+        if instance.public_ip_address:
+            ips.append(instance.public_ip_address)
+        else:
+            ips.append(instance.private_ip_address)
 
 with open('shared/ips.txt', 'a') as f:
     f.writelines("%s\n" % ip for ip in ips)
